b79d60d629feb381b7140b0fc89efac636730ceb
hereiam123 <bdemaio1@gmail.com>
2019-09-21 20:33:01 -0400

    Setting up session handling for client

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,30 @@ const cors = require('cors')({
 })
 const admin = require("firebase-admin")
 const serviceAccount = require("./service-account.json")
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://chatbot-52280.firebaseio.com"
+});
+
+const {
+    SessionsClient
+} = require("dialogflow")
+
+exports.dialogflowGateway = functions.https.onRequest((request, response) => {
+    cors(request, response, async () => {
+
+        //Query input is the information passed from the user
+        //Audio or text data
+        const {
+            queryInput,
+            sessionId
+        } = request.body
+
+        const sessionClient = new SessionsClient({
+            credentials: serviceAccount
+        })
+
+        const session = sessionClient.sessionPath("Chatbot", sessionId)
+    })
+})
