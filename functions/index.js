d1214b5b9f0e893bc628c6959994cadffe78fc49
hereiam123 <bdemaio1@gmail.com>
2019-09-21 23:29:18 -0400

    Connected with angulkar frontend, working pretty decently

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,9 @@ const cors = require('cors')({
 })
 const admin = require("firebase-admin")
 const serviceAccount = require("./service-account.json")
+const {
+    WebhookClient
+} = require("dialogflow-fulfillment")
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -41,4 +44,44 @@ exports.dialogflowGateway = functions.https.onRequest((request, response) => {
 
         response.send(result)
     })
+})
+
+exports.dialogflowWebhook = functions.https.onRequest(async (request, response) => {
+    const agent = new WebhookClient({
+        request,
+        response
+    })
+
+    console.log(JSON.stringify(request.body))
+
+    const result = request.body.queryResult
+
+    function welcome(agent) {
+        agent.add("Welcome to my agent!")
+    }
+
+    function fallback(agent) {
+        agent.add("Sorry can you try again?")
+    }
+
+    async function userOnboardingHandler(agent) {
+        const db = admin.firestore()
+        const profile = db.collection('users').doc("jeffd23")
+        const {
+            name,
+            color
+        } = result.parameters
+
+        await profile.set({
+            name,
+            color
+        })
+
+        agent.add("Welcome aboard!")
+    }
+
+    let intentMap = new Map()
+    intentMap.set("Default Welcome Intent", welcome)
+    intentMap.set("Default Fallback Intent", fallback)
+    intentMap.set("Update Username", userOnboardingHandler)
 })
